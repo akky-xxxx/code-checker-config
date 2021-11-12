903f86227f1506f219074c9ff977cd3d42086cde
akky--xxxx <114.s.aki@gmail.com>
2021-11-12 18:57:21 +0900

    chore: add eslint rule of ts

--- configs/eslint/ts.js
+++ configs/eslint/ts.js
@@ -48,6 +48,10 @@ module.exports = {
       WARN,
       { checkIntersections: true },
     ],
+    "@typescript-eslint/consistent-type-assertions": [
+      ERROR,
+      { assertionStyle: "never" },
+    ],
   },
   settings: {
     "import/resolver": {
